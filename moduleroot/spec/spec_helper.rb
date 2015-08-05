6ae4fa942b26db31e0532c43d39f236d1e415510
Cody Herriges <cody@herriges.org>
2015-08-05 09:11:23 -0700

    Add helper for concat verification
      This commit will add a helper to spec_helper.rb that is used to assist
      in the validation of concat::fragment resources.
    
      Without this, puppet-horizon can not run tests successfully.
    
    Change-Id: Icc267b02be87357fdf45f8125f622b28e7d378aa

--- moduleroot/spec/spec_helper.rb
+++ moduleroot/spec/spec_helper.rb
@@ -7,4 +7,9 @@ RSpec.configure do |c|
   c.alias_it_should_behave_like_to :it_raises, 'raises'
 end
 
+def verify_concat_fragment_contents(subject, title, expected_lines)
+  content = subject.resource('concat::fragment', title).send(:parameters)[:content]
+  expect(content.split("\n") & expected_lines).to eq(expected_lines)
+end
+
 at_exit { RSpec::Puppet::Coverage.report! }
