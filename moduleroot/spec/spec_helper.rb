91f0de5aac1fac09a5daf9c4a2810ddd1002c488
Alex Schultz <aschultz@mirantis.com>
2016-07-11 11:45:51 -0600

    CI test
    Using this to try and debug cookiecutter
    
    plzdon'tmerge
    
    Change-Id: Ia71098a9d47f2dc73b6f90d3601a1140b6be3314

--- moduleroot/spec/spec_helper.rb
+++ moduleroot/spec/spec_helper.rb
@@ -8,6 +8,7 @@ include RspecPuppetFacts
 RSpec.configure do |c|
   c.alias_it_should_behave_like_to :it_configures, 'configures'
   c.alias_it_should_behave_like_to :it_raises, 'raises'
+  c.backtrace_exclusion_patterns = []
 end
 
 at_exit { RSpec::Puppet::Coverage.report! }
